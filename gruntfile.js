c0bc877fb0fd0bcc78b02763220e23b8a6aae676
sollomonn <sollomonn4@gmail.com>
2019-12-27 20:31:37 +0200

    grunt

--- gruntfile.js
+++ gruntfile.js
@@ -23,10 +23,6 @@ module.exports = grunt => {
 				' */'
 		},
 
-		qunit: {
-			files: [ 'test/*.html' ]
-		},
-
 		uglify: {
 			options: {
 				banner: '<%= meta.banner %>\n',
@@ -145,10 +141,6 @@ module.exports = grunt => {
 				files: [ 'css/reveal.scss' ],
 				tasks: 'css-core'
 			},
-			test: {
-				files: [ 'test/*.html' ],
-				tasks: 'test'
-			},
 			html: {
 				files: root.map(path => path + '/*.html')
 			},
@@ -166,7 +158,7 @@ module.exports = grunt => {
 	grunt.registerTask( 'default', [ 'css', 'js' ] );
 
 	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+	grunt.registerTask( 'js', [ 'jshint', 'uglify' ] );
 
 	// Theme CSS
 	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
@@ -182,8 +174,4 @@ module.exports = grunt => {
 
 	// Serve presentation locally
 	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
-
-	// Run tests
-	grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
-
 };
